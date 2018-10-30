eb99805b6c0206bb3a4a5fefa9d9f7ccec26f95a
Jonathan Viray <jfviray7@gmail.com>
2018-10-29 23:58:47 -0400

    Initialize repo

---
+++ ethereum/compile.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const solc = require('solc'); // Solidity compiler
+const fs = require('fs-extra'); // Gives access to file system
+
+// Deletes the entire build folder if it already exists
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+// Retrieves contents of a specific smart contract
+const CONTRACTPath = path.resolve(__dirname, 'contracts', 'CONTRACT.sol');
+const source = fs.readFileSync(CONTRACTPath, 'utf-8');
+
+// Compiles source and extracts 'contracts' object
+const output = solc.compile(source, 1).contracts;
+
+// Re-creates build folder if it does not already exist
+fs.ensureDirSync(buildPath);
+
+// Iterates over each key (contract) in the 'contracts' object (output)
+for (let contract in output) {
+  // Writes a JSON file to a specified location using the key for the filename
+  // and specified contents from the 'contracts' object (output)
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract.replace(':', '') + '.json'),
+    output[contract]
+  )
+}
---
+++ test/CONTRACT.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+// Code below will ignore the MaxListenersExceededWarning from the compiler
+// It's a bug in either web3 or the provider and cannot be fixed
+require('events').EventEmitter.defaultMaxListeners = 0;
+
+const compiledCONTRACT = require('../ethereum/build/CONTRACT.json');
