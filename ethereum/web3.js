322757177c86c16e3e2c1c464b76e0f76d3fc071
Jonathan Viray <jfviray7@gmail.com>
2019-01-29 21:59:31 -0500

    Update web3 file

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,7 +1,25 @@
 import Web3 from 'web3';
 
-// Assume users have metamask installed
-// Pass Web3 the provider automatically injected into your browswer by Metamask
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+  async function requestAccess() {
+    web3 = new Web3(window.ethereum);
+    await ethereum.enable();
+  }
+
+  requestAccess();
+} else if (
+  typeof window !== 'undefined' &&
+  typeof window.web3 !== 'undefined'
+) {
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  const provider = new Web3.providers.HttpProvider(
+    'https://rinkeby.infura.io/v3/c58307de419b44e2a42cd272d47a08e3'
+  );
+
+  web3 = new Web3(provider);
+}
 
 export default web3;
--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Head from 'next/head';
 import { Jumbotron, Button } from 'react-bootstrap/lib';
+import web3 from '../ethereum/web3';
 
-export default () => {
-  const jumbostyle = {
-    textAlign: "center",
-    marginTop: "30px"
-  };
+class Welcome extends React.Component {
+  static async getInitialProps() {
+    const accounts = await web3.eth.getAccounts();
 
-  return (
-    <div>
-      <Head>
-        <title>BUIDL</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        {/* -- Latest compiled and minified CSS */}
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-          crossorigin="anonymous"
-        />
-      </Head>
+    return { accounts };
+  }
 
-      {/* NAVBAR */}
-      <nav className="navbar navbar-inverse navbar-fixed-top">
-        <div className="container">
-          <div className="navbar-header">
-            <a href="#" className="navbar-brand">BUIDL</a>
+  render() {
+    const jumbostyle = {
+      textAlign: 'center',
+      marginTop: '30px'
+    };
+
+    return (
+      <div>
+        <Head>
+          <title>BUIDL</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          {/* -- Latest compiled and minified CSS */}
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+            integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+            crossorigin="anonymous"
+          />
+        </Head>
+        {/* NAVBAR */}
+        <nav className="navbar navbar-inverse navbar-fixed-top">
+          <div className="container">
+            <div className="navbar-header">
+              <a href="#" className="navbar-brand">
+                BUIDL
+              </a>
+            </div>
+
+            {/* NAVBAR ITEMS */}
+            <ul className="nav navbar-nav navbar-right">
+              <li className="navbar-brand">{this.props.accounts[0]}</li>
+            </ul>
           </div>
-        </div>
-      </nav>
+        </nav>
+        <Jumbotron>
+          <div className="container" style={jumbostyle}>
+            <h1>Welcome, Ethereum Devs!</h1>
+            <p>
+              As a community we need to start building new and powerful
+              applications that truly add value.
+            </p>
+            <p>
+              <a href="https://www.coindesk.com/dont-hodl-buidl-blockchain-tech-will-add-value-2018">
+                <Button bsStyle="primary">Learn more</Button>
+              </a>
+            </p>
+          </div>
+        </Jumbotron>;
+      </div>
+    );
+  }
+}
 
-      <Jumbotron>
-        <div className="container" style={jumbostyle}>
-          <h1>Welcome, Ethereum Devs!</h1>
-          <p>
-            As a community we need to start building new and powerful applications that truly add value.
-          </p>
-          <p>
-            <a href="https://www.coindesk.com/dont-hodl-buidl-blockchain-tech-will-add-value-2018">
-              <Button bsStyle="primary">
-                Learn more
-              </Button>
-            </a>
-          </p>
-        </div>
-      </Jumbotron>;
-    </div>
-  );
-};
+export default Welcome;
